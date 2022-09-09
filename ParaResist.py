044dd6af5565d92085462153457b9c0237f957cc
Jack Maguire <jackmaguire1234@gmail.com>
2022-09-09 13:51:48 +0100

    add physics para resist

---
+++ ParaResist.py
@@ -0,0 +1,19 @@
+numbers = []
+while True:
+    inp = input("Enter a number: ")
+    if inp == "." or inp == "":
+        break
+    
+    try:
+        n = int(inp)
+        numbers.append(n)
+    except:
+        print("Invalid, Exiting")
+        break
+
+
+total = 0.0
+for n in numbers:
+    total += 1.0 / n
+
+print("Parallel Resistance is ", 1.0 / total)
